9d4a43d3a9fe3ac1504e46078d2795fca1ad25f5
Rud1ge <rudige@yandex.ru>
2026-02-01 15:17:23 +0700

    S/d ports are fixed

--- scripts/traffic.py
+++ scripts/traffic.py
@@ -1,18 +1,19 @@
-from scapy.all import *
+from scapy.all import IP, UDP, Raw, RandIP, send
+
+SPORT = 12345
+DPORT = 54321
+PACKETS = 1024
 
 
 def generate_and_send():
     packets = []
-    for _ in range(1024):
-        pkt = (
-            IP(dst="172.16.0.254", src=RandIP())
-            / UDP(sport=RandShort(), dport=RandShort())
-            / Raw(load="ECMP Hash Testing for b4com" * 64)
-        )
+    payload = Raw(load=("ECMP Hash Testing for b4com" * 64).encode())
+
+    for packet in range(PACKETS):
+        pkt = IP(dst="172.16.0.254", src=RandIP()) / UDP(sport=SPORT, dport=DPORT) / payload
         packets.append(pkt)
 
-    send(packets, inter=0, iface=None)
-    print("Информация: Закончилась отправка случайных UDP-пакетов")
+    send(packets, inter=0.01, iface="eth0", verbose=False)
 
 
 if __name__ == "__main__":
